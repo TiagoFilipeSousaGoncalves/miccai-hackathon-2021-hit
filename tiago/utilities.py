2519c46d4db98a855634edcf1caf6beac4038527
Tiago Gonçalves <tiago.galves@hotmail.com>
2021-10-24 10:18:07 +0100

    Added new stuff to AEBackboneClf

--- tiago/utilities.py
+++ tiago/utilities.py
@@ -213,7 +213,7 @@ class AEBackboneClf(torch.nn.Module):
 
 
         # Apply GlobalAveragePooling
-        self.globalaveragepool = torch.nn.AvgPool2d(kernel_size=_in_features.size(1))
+        self.globalaveragepool = torch.nn.AvgPool2d(kernel_size=_in_features.size(2))
 
 
         # FC-Layers
